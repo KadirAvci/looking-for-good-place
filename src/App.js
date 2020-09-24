f565476b5527e1905cc973270173495e4ee9288e
avci kadir <contact@avci-kadir.com>
2020-09-24 13:52:39 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Site from './containers/Site/Site'
+
+class App extends Component {
+    render() {
+        return (
+            <Site/>
+        );
+    }
+}
+
+export default App;
